c1cec504fe31c72b0a9b531478742124057a387c
marco <maldonmarco@gmail.com>
2015-04-20 23:48:40 -0430

    Qmatic

---
+++ js/Qmatic.js
@@ -0,0 +1,68 @@
+
+var cola1 = new Array(200);
+var contCola1 = 400;
+function agregaCola1(){
+   cola1.push(contCola1);
+   contCola1++;
+   console.log(cola1);
+
+}
+
+var cola1 = new Array(200);
+var contCola1 = 400;
+function quitarCola1(){
+   cola1.shift(contCola1);
+   contCola1++;
+   console.log(cola1); 
+}
+
+var cola2 = new Array(200);
+var contCola2 = 200;
+function agregaCola2(){
+   cola2.push(contCola2);
+   contCola2++;
+   console.log(cola2);
+
+}
+
+var cola2 = new Array(200);
+var contCola2 = 200;
+function quitarCola2(){
+   cola2.shift(contCola2);
+   contCola2++;
+   console.log(cola2); 
+}
+
+var cola3 = new Array(200);
+var contCola3 = 600;
+function agregaCola3(){
+   cola3.push(contCola3);
+   contCola3++;
+   console.log(cola3);
+
+}
+
+var cola3 = new Array(200);
+var contCola3 = 600;
+function quitarCola3(){
+   cola3.shift(contCola3);
+   contCola3++;
+   console.log(cola3); 
+}
+
+var cola4 = new Array(200);
+var contCola4 = 800;
+function agregaCola4(){
+   cola4.push(contCola4);
+   contCola4++;
+   console.log(cola4);
+
+}
+
+var cola4 = new Array(200);
+var contCola4 = 800;
+function quitarCola4(){
+   cola4.shift(contCola4);
+   contCola4++;
+   console.log(cola4); 
+}
